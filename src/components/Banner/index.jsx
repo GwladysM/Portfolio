0f92014bbd53b9bf5eca1581fb58f36d93c0695c
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-10-01 20:46:02 -0400

    responsive

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,13 +3,15 @@ import './banner.scss';
 
 function Banner() {
     return (
-        <div id="home" className="banner" style={{
+        <div id="home" className="banner__container" style={{
             backgroundImage: 'linear-gradient(rgba(219, 249, 216, 1), rgba(255, 255, 255, 0.3)), url(./assets/winston-tjia-JZvoDS6JHTU-unsplash.jpg)',
             backgroundSize: 'cover'
         }}>
-            <h1>Bienvenu,</h1>
-            <br />
-            <h2>Je suis <span>Gwladys </span>, Développeuse Front-end</h2>
+            <div className="banner">
+                <h1>Bienvenu,</h1>
+                <br />
+                <h2>Je suis <span>Gwladys </span>, Développeuse Front-end</h2>
+            </div>
         </div>
     )
 }
