ff85084a652ef2b9df86c30d5516934237895c71
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-10-02 13:04:15 -0400

    modif route

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,29 +10,29 @@ function Header() {
 
     return (
         <nav className='navbar'>
-            <a href='/Portfolio/#home' className='navbar__logo'>
+            <a href='/#home' className='navbar__logo'>
                 <Logo />
             </a>
             <div className='navbar__navlink'>
-                <a href="/Portfolio/#home"
+                <a href="/#home"
                     className={active === 'home' ? "" : ""}
                     onClick={() => handleClick('home')}
                 >
                     Accueil
                 </a>
-                <a href="/Portfolio/#about"
+                <a href="/#about"
                     className={active === 'about' ? "active" : ""}
                     onClick={() => handleClick('about')}
                 >
                     A propos
                 </a>
-                <a href="/Portfolio/#projects"
+                <a href="/#projects"
                     className={active === 'projects' ? "active" : ""}
                     onClick={() => handleClick('projects')}
                 >
                     Projets
                 </a>
-                <a href="/Portfolio/#contact"
+                <a href="/#contact"
                     className={active === 'contact' ? "active" : ""}
                     onClick={() => handleClick('contact')}
                 >
