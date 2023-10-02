1f577c2bc8bb69788289613f085abcea203282b0
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-10-02 13:37:43 -0400

    ajout basename au router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='Portfolio'>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
