a5b46379a1c65a7338d317dc771b35e091c8f7b7
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-11-20 16:06:54 -0400

    MAJ CV, résolution pb dimensions responsive

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,15 +11,15 @@ function About() {
                 Je suis Gwladys, la trentaine,
                 originaire du bassin Rennais, je vis désormais en Guadeloupe.
                 <br />
-                Mon parcours a changé de direction récemment, lorsque
+                Mon parcours a changé en début d'année 2023, lorsque
                 j'ai choisi d'effectuer une reconversion professionnelle après
                 dix belles années en tant qu'assistante spécialisée vétérinaire.
                 <br />
-                Je me forme donc auprès d'OpenClassRoom afin d'obtenir le diplôme certifiant de développeur web.
+                Je me suis donc formée pendant huit mois auprès d'OpenClassRoom afin d'obtenir le diplôme certifiant de développeur web.
                 <br />
-                Au cours des huit derniers mois, j'ai appris différents langages de programmation,
+                Au cours de cette formation, j'ai appris différents langages de programmation,
                 notamment HTML, CSS, Javascript et plus récemment React.
-                Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO font également partie de ma formation.
+                Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO ont également fait partie de ma formation.
                 <br />
                 Mon objectif désormais est de progresser et me perfectionner sur la partie front-end et en particulier via React.
             </p>
