d883fdddea222d40acdf3b4f32698c397a271d8a
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2024-01-03 12:38:30 -0400

    reprise codes couleurs et logo

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,10 +3,7 @@ import './banner.scss';
 
 function Banner() {
     return (
-        <div id="home" className="banner__container" style={{
-            backgroundImage: 'linear-gradient(rgba(219, 249, 216, 1), rgba(255, 255, 255, 0.3)), url(./assets/winston-tjia-JZvoDS6JHTU-unsplash.jpg)',
-            backgroundSize: 'cover'
-        }}>
+        <div id="home" className="banner__container">
             <div className="banner">
                 <h1>Bienvenue,</h1>
                 <br />
