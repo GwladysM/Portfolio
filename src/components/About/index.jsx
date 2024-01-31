1d1b636f433ad1d538ca991cf785d6837ab2eb23
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2024-01-30 20:03:21 -0400

    ajout photo profil + modif about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,32 +6,39 @@ function About() {
         <div id="about" className="about">
             <h2>Qui suis-je ?</h2>
             <p className="about__description">
-                Hello,
-                <br />
-                Je suis Gwladys, la trentaine,
-                originaire du bassin Rennais, je vis désormais en Guadeloupe.
-                <br />
-                Mon parcours a changé en début d'année 2023, lorsque
-                j'ai choisi d'effectuer une reconversion professionnelle après
-                dix belles années en tant qu'assistante spécialisée vétérinaire.
-                <br />
-                Je me suis donc formée pendant huit mois auprès d'OpenClassRoom afin d'obtenir le diplôme certifiant de développeur web.
-                <br />
-                Au cours de cette formation, j'ai appris différents langages de programmation,
-                notamment HTML, CSS, Javascript et plus récemment React.
-                <br />
-                Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO ont également fait partie de ma formation.
-                Dans la partie <a href="#projects">
-                    <span style={{
-                        color: "black",
-                        textShadow: "none",
-                        textTransform: "uppercase",
-                        fontWeight: "normal",
-                        fontSize: "22px"
-                    }}>mes projets</span>
-                </a>, vous retrouverez les travaux que j'ai réalisé.
-                <br />
-                Mon objectif désormais est de progresser et me perfectionner sur la partie front-end et en particulier via React.
+                <div className="left">
+                    Hello,
+                    <br />
+                    Je suis Gwladys, la trentaine,
+                    originaire du bassin Rennais, je vis désormais en Guadeloupe.
+                    <br />
+                    Mon parcours a changé en début d'année 2023, lorsque
+                    j'ai choisi d'effectuer une reconversion professionnelle après
+                    dix belles années en tant qu'assistante spécialisée vétérinaire.
+                    <br />
+                </div>
+                <div className="right">
+                    Je me suis donc formée pendant huit mois auprès d'OpenClassRoom afin d'obtenir le diplôme certifiant de développeur web.
+                    <br />
+                    Au cours de cette formation, j'ai appris différents langages de programmation,
+                    notamment HTML, CSS, Javascript et plus récemment React.
+                    <br />
+                    Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO ont également fait partie de ma formation.
+                    Dans la partie <a href="#projects">
+                        <span style={{
+                            color: "black",
+                            textShadow: "none",
+                            textTransform: "uppercase",
+                            fontWeight: "normal",
+                            fontSize: "22px"
+                        }}>mes projets</span>
+                    </a>, vous retrouverez les travaux que j'ai réalisé.
+                    <br />
+                </div>
+                <img className="about_photo" src="./assets/Profil-small.webp" alt="Profil" />
+                <div className="center">
+                    Mon objectif désormais est de progresser et me perfectionner sur la partie front-end et en particulier via React.
+                </div>
             </p>
         </div>
     )
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -4,12 +4,12 @@ import './banner.scss';
 function Banner() {
     return (
         <div id="home" className="banner__container" style={{
-            backgroundImage: 'linear-gradient(rgba(167, 201, 87, 0.8), rgba(237, 229, 116, 0.8)), url(./assets/winston-tjia-3jvqWh1A0eQ-unsplash.jpg)',
+            backgroundImage: 'linear-gradient(to top, rgba(157, 164, 142, 0.8), rgba(245, 245, 245, 0.8)), url(./assets/winston-tjia-3jvqWh1A0eQ-unsplash.jpg)',
             backgroundSize: 'cover'
         }}>
             <div className="banner">
                 <h1>Gwladys Morlier</h1>
-                <h2>Développeuse Front-End</h2>
+                <h2>Développeuse Web</h2>
             </div>
         </div>
     )
