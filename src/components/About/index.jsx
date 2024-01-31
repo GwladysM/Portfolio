1b8d66cb3eef43ad952eed054d92eb3b89c7aa2d
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2024-01-30 22:19:06 -0400

    creation blocs et bulles about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,41 +5,48 @@ function About() {
     return (
         <div id="about" className="about">
             <h2>Qui suis-je ?</h2>
-            <p className="about__description">
-                <div className="left">
-                    Hello,
-                    <br />
-                    Je suis Gwladys, la trentaine,
-                    originaire du bassin Rennais, je vis désormais en Guadeloupe.
-                    <br />
-                    Mon parcours a changé en début d'année 2023, lorsque
-                    j'ai choisi d'effectuer une reconversion professionnelle après
-                    dix belles années en tant qu'assistante spécialisée vétérinaire.
-                    <br />
+            <div className="about__description">
+                <div className="top">
+                    <div className="left">
+                        Bienvenue sur mon Portfolio,
+                        <br />
+                        Je suis Gwladys, la trentaine,
+                        originaire du bassin Rennais, je vis désormais en Guadeloupe.
+                        <br />
+                        Mon parcours a changé en 2023, lorsque
+                        j'ai choisi d'effectuer une reconversion professionnelle après
+                        dix belles années en tant qu'assistante spécialisée vétérinaire.
+                    </div>
+                    <div className="nothing"></div>
                 </div>
-                <div className="right">
-                    Je me suis donc formée pendant huit mois auprès d'OpenClassRoom afin d'obtenir le diplôme certifiant de développeur web.
-                    <br />
-                    Au cours de cette formation, j'ai appris différents langages de programmation,
-                    notamment HTML, CSS, Javascript et plus récemment React.
-                    <br />
-                    Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO ont également fait partie de ma formation.
-                    Dans la partie <a href="#projects">
-                        <span style={{
-                            color: "black",
-                            textShadow: "none",
-                            textTransform: "uppercase",
-                            fontWeight: "normal",
-                            fontSize: "22px"
-                        }}>mes projets</span>
-                    </a>, vous retrouverez les travaux que j'ai réalisé.
-                    <br />
+                <div className="middle">
+                    <div className="right">
+                        Formée chez OpenClassRoom, j'ai obtenu le diplôme certifiant de développeur web.
+                        <br />
+                        Au cours de cette formation, j'ai appris différents langages de programmation,
+                        notamment HTML, CSS, Javascript et plus récemment React.
+                        <br />
+                        Mais ce n'est pas tout, apprendre comment optimiser un site web, améliorer son référencement et son SEO ont également fait partie de ma formation.
+                        <br />
+                        Dans la partie <a href="#projects">
+                            <span style={{
+                                color: "black",
+                                textShadow: "none",
+                                textTransform: "uppercase",
+                                fontWeight: "normal",
+                                fontSize: "20px"
+                            }}>mes projets</span>
+                        </a>, vous retrouverez les travaux que j'ai réalisé.
+                    </div>
+                    <img className="about_photo" src="./assets/Profil-small.webp" alt="Profil" />
                 </div>
-                <img className="about_photo" src="./assets/Profil-small.webp" alt="Profil" />
-                <div className="center">
-                    Mon objectif désormais est de progresser et me perfectionner sur la partie front-end et en particulier via React.
+                <div className="bottom">
+                    <div className="center">
+                        Mon objectif désormais est de progresser et me perfectionner sur la partie front-end et en particulier via React.
+                    </div>
+                    <div className="nothing"></div>
                 </div>
-            </p>
+            </div>
         </div>
     )
 }
